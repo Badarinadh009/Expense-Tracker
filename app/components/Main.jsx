9ac397064252124a7513a2964f3cc4a7a61a517b
badri <badrinadh4641@gmail.com>
2025-06-17 17:17:03 +0530

    commit

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -37,21 +37,21 @@ function Main() {
             <p>Enter the type of entry and the amount to add it to expense</p>
             <div className='h-[200px] flex items-end justify-center gap-6 pb-2'>
                 <input className='border-2' type='text' placeholder='Enter the type' value={text} onChange={(e)=>setText(e.target.value)} />
-                <input className='border-2' type='number' placeholder='Enter the amount' value={amount} onChange={(e)=>setAmount(e.target.value)} />
+                <input className='border-2' onKeyDown={(e)=>e.key==='Enter'? addTransaction :''} type='number' placeholder='Enter the amount' value={amount} onChange={(e)=>setAmount(e.target.value)} />
             </div>
             <button  className='bg-black w-[80px] text-white rounded-2xl'onClick={addTransaction}>Add</button>
         </div>
       <div className='bg-black text-amber-400 p-4'>
-        <div className='flex justify-between font-bold text-xl h-[60px]'>
+        <div className='flex justify-between font-bold text-xl h-[100px]'>
             <h1>Your total balance: {getBalance()}</h1>
             <h1>Your total Expense: {getExpense()}</h1>
             <h1>Your total Income: {getIncome()}</h1>
         </div>
         <ul className='grid grid-cols-3 pt-4'>
             {transaction.map((value, index)=>(
-                <li key={index}><span>{value.text}</span>
+                <li key={index}><span>{value.text}</span>-
                 <span>{value.amount}</span>
-                <span  className='cursor-pointer'onClick={()=>deleteTransaction(value.id)}>X</span></li>
+                <span  className='cursor-pointer pl-2'onClick={()=>deleteTransaction(value.id)}>X</span></li>
             ))}
         </ul>
       </div>
